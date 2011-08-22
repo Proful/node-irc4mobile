ec528d18d7a66ea6405f4e0453600ff9824cc2a1
Butu <butu25@gmail.com>
2011-08-22 12:16:21 +0530

    working.. able to display msg on browser

--- public/js/client.coffee
+++ public/js/client.coffee
@@ -2,3 +2,9 @@ socket = io.connect "http://localhost"
 
 socket.on "msg", (data) ->
   console.log data
+  source = $("#msg-tmpl").html()
+  console.log source
+  source = source.replace(/\\n/g,"")
+  template = Handlebars.compile(source)
+  html = template(data)
+  $('#msgs').append(html)
--- public/js/client.js
+++ public/js/client.js
@@ -2,6 +2,13 @@
   var socket;
   socket = io.connect("http://localhost");
   socket.on("msg", function(data) {
-    return console.log(data);
+    var html, source, template;
+    console.log(data);
+    source = $("#msg-tmpl").html();
+    console.log(source);
+    source = source.replace(/\\n/g, "");
+    template = Handlebars.compile(source);
+    html = template(data);
+    return $('#msgs').append(html);
   });
 }).call(this);
